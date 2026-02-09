f8124e243d1db576a288b418083ad28ba71f367a
EuroCase007 <nicolaspollio97@gmail.com>
2026-02-09 23:55:41 +0100

    add FeignClient

--- department-service/src/main/java/com/example/department_service/model/Employee.java
+++ department-service/src/main/java/com/example/department_service/model/Employee.java
@@ -1,10 +1,14 @@
 package com.example.department_service.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     private Long id;
